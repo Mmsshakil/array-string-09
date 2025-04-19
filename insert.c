217280a14415ad4ab4e61cbaca83b0a15e6aca3b
Mmsshakil <softhandshakil@gmail.com>
2025-04-19 23:35:53 +0600

    array insert basic

--- insert.c
+++ insert.c
@@ -3,7 +3,7 @@ int main()
 {
     int n, indx, val;
     scanf("%d", &n);
-    int arr[n];
+    int arr[n + 1];
 
     for (int i = 0; i < n; i++)
     {
@@ -11,6 +11,18 @@ int main()
     }
 
     scanf("%d %d", &indx, &val);
+
+    for (int i = n; i >= indx + 1; i--) // this is very important
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[indx] = val;
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 
     return 0;
 }
